eefd60e9b9a1765f933b9d48a421ff35dfb3f61f
Aaditya1273 <aadityaiitian1515@gmail.com>
2026-08-12 21:16:02 +0530

    Add page.tsx

---
+++ frontend/app/app/shield/page.tsx
@@ -0,0 +1,9 @@
+import { ShieldCard } from "@/components/ShieldCard";
+
+export default function ShieldPage() {
+  return (
+    <div className="mx-auto max-w-lg">
+      <ShieldCard />
+    </div>
+  );
+}
