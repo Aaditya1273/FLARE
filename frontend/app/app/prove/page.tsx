782960592e06d27b35c3f67600a270163035d9c7
Aaditya1273 <aadityaiitian1515@gmail.com>
2026-08-12 21:16:02 +0530

    Add page.tsx

---
+++ frontend/app/app/prove/page.tsx
@@ -0,0 +1,9 @@
+import { ProveCard } from "@/components/ProveCard";
+
+export default function ProvePage() {
+  return (
+    <div className="mx-auto max-w-lg">
+      <ProveCard />
+    </div>
+  );
+}
