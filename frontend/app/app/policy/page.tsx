eb3d4d59c3caf5398b881973816923d7002db5bf
Aaditya1273 <aadityaiitian1515@gmail.com>
2026-08-12 21:16:02 +0530

    Add page.tsx

---
+++ frontend/app/app/policy/page.tsx
@@ -0,0 +1,9 @@
+import { PrivatePolicyCard } from "@/components/PrivatePolicyCard";
+
+export default function PolicyPage() {
+  return (
+    <div className="mx-auto max-w-lg">
+      <PrivatePolicyCard />
+    </div>
+  );
+}
